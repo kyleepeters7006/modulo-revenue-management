fe9263f18810774f6c7adfeb25991802f507838a
gre502g <gregory.driver@trilogyhs.com>
2026-07-23 16:22:57 -0400

    Main.tf Update

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.78"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 # -----------------------------------------------------------------------------
 # Shared values
